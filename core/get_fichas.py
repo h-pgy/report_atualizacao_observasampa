94fb59079ade9ceaea80fe2245bc3a93072b8be9
h-pgy <hpougy@gmail.com>
2022-08-01 13:13:39 -0300

    download todas as fichas finalizado

--- core/get_fichas.py
+++ core/get_fichas.py
@@ -1,6 +1,6 @@
 from requests import session
 from bs4 import BeautifulSoup
-from .utils import solve_dir, solve_path
+from .utils import save_file, solve_dir, solve_path
 from .exceptions import FichaNotFound
 
 class FichaDownload:
@@ -29,7 +29,7 @@ class FichaDownload:
 
     def make_soup(self, html):
 
-        soup = BeautifulSoup(html)
+        soup = BeautifulSoup(html, features="lxml")
 
         return soup
 
@@ -50,6 +50,48 @@ class FichaDownload:
         return sopa
 
 
+def download_all_fichas(start=1, end=1000, max_not_found = 10, 
+                        save_dir = 'original_data', save_report=True):
+
+
+    download_ficha = FichaDownload()
+    not_found = []
+    not_found_seguido = 0
+    for i in range(start, end):
+
+        try:
+            sopa = download_ficha(i)
+            html = sopa.prettify()
+            fname = f'ficha_{i}.html'
+            save_file(html, fname, save_dir, callback='text')
+            not_found_seguido=0
+
+        except FichaNotFound as e:
+
+            not_found.append(i)
+            not_found_seguido+=1
+
+            if not_found_seguido >= max_not_found:
+                break
+
+    total_fichas = i-len(not_found)
+    print("Finalizando download das fichas"
+        f"{total_fichas} fichas baixadas")
+        
+
+    report = {'total_fichas' : total_fichas,
+            'iteracoes' : i,
+            'max_not_found' : max_not_found,
+            'total_nao_encontradas' : len(not_found),
+            'nao_encontradas' : not_found}
+    
+    print(report)
+
+    if save_report:
+        save_file(report, 'relatorio_download_fichas.json', 
+                    save_dir='generated_data', callback='json')
+
+    return report
 
 
     
