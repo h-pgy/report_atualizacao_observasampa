f3b62b32645e3f6c15c41d84781a736d7975f18a
Henrique Pougy <hpougy@gmail.com>
2022-11-21 17:13:26 -0300

    report variaveis finalizado

--- core/utils/utils.py
+++ core/utils/utils.py
@@ -1,4 +1,4 @@
-
+import os
 
 def build_conn_str(user, password, host, port, 
                        database, dbms='postgresql'):
@@ -6,3 +6,19 @@ def build_conn_str(user, password, host, port,
     conn_str = f'{dbms}://{user}:{password}@{host}:{port}/{database}'
     
     return conn_str
+
+def solve_dir(folder):
+
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+
+    return os.path.abspath(folder)
+
+
+def solve_path(path, parent=None):
+    
+    if parent:
+        parent = solve_dir(parent)
+        path = os.path.join(parent, path)
+    
+    return os.path.abspath(path)
